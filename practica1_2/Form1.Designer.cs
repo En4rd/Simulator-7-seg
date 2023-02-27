679f4c672bf22479829c4363e21fdf5375db377a
En4rd <caparrautn@gmail.com>
2023-02-26 17:27:30 -0700

    2

---
+++ practica1_2/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace practica1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.E3 = new System.Windows.Forms.Panel();
+            this.C3 = new System.Windows.Forms.Panel();
+            this.B3 = new System.Windows.Forms.Panel();
+            this.F3 = new System.Windows.Forms.Panel();
+            this.A3 = new System.Windows.Forms.Panel();
+            this.G3 = new System.Windows.Forms.Panel();
+            this.D3 = new System.Windows.Forms.Panel();
+            this.D2 = new System.Windows.Forms.Panel();
+            this.G2 = new System.Windows.Forms.Panel();
+            this.A2 = new System.Windows.Forms.Panel();
+            this.F2 = new System.Windows.Forms.Panel();
+            this.B2 = new System.Windows.Forms.Panel();
+            this.C2 = new System.Windows.Forms.Panel();
+            this.E2 = new System.Windows.Forms.Panel();
+            this.D1 = new System.Windows.Forms.Panel();
+            this.G1 = new System.Windows.Forms.Panel();
+            this.A1 = new System.Windows.Forms.Panel();
+            this.F1 = new System.Windows.Forms.Panel();
+            this.B1 = new System.Windows.Forms.Panel();
+            this.C1 = new System.Windows.Forms.Panel();
+            this.E1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // E3
+            // 
+            this.E3.Location = new System.Drawing.Point(145, 127);
+            this.E3.Name = "E3";
+            this.E3.Size = new System.Drawing.Size(10, 33);
+            this.E3.TabIndex = 0;
+            // 
+            // C3
+            // 
+            this.C3.Location = new System.Drawing.Point(211, 127);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(10, 33);
+            this.C3.TabIndex = 1;
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(211, 77);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(10, 33);
+            this.B3.TabIndex = 2;
+            // 
+            // F3
+            // 
+            this.F3.Location = new System.Drawing.Point(145, 77);
+            this.F3.Name = "F3";
+            this.F3.Size = new System.Drawing.Size(10, 33);
+            this.F3.TabIndex = 2;
+            // 
+            // A3
+            // 
+            this.A3.Location = new System.Drawing.Point(161, 61);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(43, 10);
+            this.A3.TabIndex = 3;
+            // 
+            // G3
+            // 
+            this.G3.Location = new System.Drawing.Point(161, 116);
+            this.G3.Name = "G3";
+            this.G3.Size = new System.Drawing.Size(43, 10);
+            this.G3.TabIndex = 4;
+            // 
+            // D3
+            // 
+            this.D3.Location = new System.Drawing.Point(161, 166);
+            this.D3.Name = "D3";
+            this.D3.Size = new System.Drawing.Size(43, 10);
+            this.D3.TabIndex = 4;
+            // 
+            // D2
+            // 
+            this.D2.Location = new System.Drawing.Point(379, 166);
+            this.D2.Name = "D2";
+            this.D2.Size = new System.Drawing.Size(43, 10);
+            this.D2.TabIndex = 10;
+            // 
+            // G2
+            // 
+            this.G2.Location = new System.Drawing.Point(379, 116);
+            this.G2.Name = "G2";
+            this.G2.Size = new System.Drawing.Size(43, 10);
+            this.G2.TabIndex = 11;
+            // 
+            // A2
+            // 
+            this.A2.Location = new System.Drawing.Point(379, 61);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(43, 10);
+            this.A2.TabIndex = 9;
+            // 
+            // F2
+            // 
+            this.F2.Location = new System.Drawing.Point(363, 77);
+            this.F2.Name = "F2";
+            this.F2.Size = new System.Drawing.Size(10, 33);
+            this.F2.TabIndex = 7;
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(429, 77);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(10, 33);
+            this.B2.TabIndex = 8;
+            // 
+            // C2
+            // 
+            this.C2.Location = new System.Drawing.Point(429, 127);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(10, 33);
+            this.C2.TabIndex = 6;
+            // 
+            // E2
+            // 
+            this.E2.Location = new System.Drawing.Point(363, 127);
+            this.E2.Name = "E2";
+            this.E2.Size = new System.Drawing.Size(10, 33);
+            this.E2.TabIndex = 5;
+            // 
+            // D1
+            // 
+            this.D1.Location = new System.Drawing.Point(581, 166);
+            this.D1.Name = "D1";
+            this.D1.Size = new System.Drawing.Size(43, 10);
+            this.D1.TabIndex = 10;
+            // 
+            // G1
+            // 
+            this.G1.Location = new System.Drawing.Point(581, 116);
+            this.G1.Name = "G1";
+            this.G1.Size = new System.Drawing.Size(43, 10);
+            this.G1.TabIndex = 11;
+            // 
+            // A1
+            // 
+            this.A1.Location = new System.Drawing.Point(581, 61);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(43, 10);
+            this.A1.TabIndex = 9;
+            // 
+            // F1
+            // 
+            this.F1.Location = new System.Drawing.Point(565, 77);
+            this.F1.Name = "F1";
+            this.F1.Size = new System.Drawing.Size(10, 33);
+            this.F1.TabIndex = 7;
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(631, 77);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(10, 33);
+            this.B1.TabIndex = 8;
+            // 
+            // C1
+            // 
+            this.C1.Location = new System.Drawing.Point(631, 127);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(10, 33);
+            this.C1.TabIndex = 6;
+            // 
+            // E1
+            // 
+            this.E1.Location = new System.Drawing.Point(565, 127);
+            this.E1.Name = "E1";
+            this.E1.Size = new System.Drawing.Size(10, 33);
+            this.E1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(326, 327);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(485, 408);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 507);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.D1);
+            this.Controls.Add(this.D2);
+            this.Controls.Add(this.G1);
+            this.Controls.Add(this.D3);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.F1);
+            this.Controls.Add(this.G2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.G3);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.E1);
+            this.Controls.Add(this.F2);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.F3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.E2);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.E3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel E3;
+        private System.Windows.Forms.Panel C3;
+        private System.Windows.Forms.Panel B3;
+        private System.Windows.Forms.Panel F3;
+        private System.Windows.Forms.Panel A3;
+        private System.Windows.Forms.Panel G3;
+        private System.Windows.Forms.Panel D3;
+        private System.Windows.Forms.Panel D2;
+        private System.Windows.Forms.Panel G2;
+        private System.Windows.Forms.Panel A2;
+        private System.Windows.Forms.Panel F2;
+        private System.Windows.Forms.Panel B2;
+        private System.Windows.Forms.Panel C2;
+        private System.Windows.Forms.Panel E2;
+        private System.Windows.Forms.Panel D1;
+        private System.Windows.Forms.Panel G1;
+        private System.Windows.Forms.Panel A1;
+        private System.Windows.Forms.Panel F1;
+        private System.Windows.Forms.Panel B1;
+        private System.Windows.Forms.Panel C1;
+        private System.Windows.Forms.Panel E1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ practica1_2/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica1_2
+{
+    
+    public partial class Form1 : Form
+    {
+        string Num;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Num = textBox1.Text.PadLeft(3, '0');
+            label1.Text = Num;
+            switch (Convert.ToString(Num[0]))
+            {
+                case "0":
+                    A3.BackColor = Color.Red;
+                    B3.BackColor = Color.Red;
+                    C3.BackColor = Color.Red;
+                    D3.BackColor = Color.Red;
+                    E3.BackColor = Color.Red;
+                    F3.BackColor = Color.Red;
+                    G3.BackColor = Color.Gainsboro;
+                    break;
+
+
+                case "1":
+                    A3.BackColor = Color.Gainsboro;
+                    B3.BackColor = Color.Red;
+                    C3.BackColor = Color.Red;
+                    D3.BackColor = Color.Gainsboro;
+                    E3.BackColor = Color.Gainsboro;
+                    F3.BackColor = Color.Gainsboro;
+                    G3.BackColor = Color.Gainsboro;
+                    break;
+
+                case "2":
+                    A3.BackColor = Color.Red;
+                    B3.BackColor = Color.Red;
+                    C3.BackColor = Color.Gainsboro;
+                    D3.BackColor = Color.Red;
+                    E3.BackColor = Color.Red;
+                    F3.BackColor = Color.Gainsboro;
+                    G3.BackColor = Color.Red;
+                    break;
+
+                case "3":
+                    A3.BackColor = Color.Red;
+                    B3.BackColor = Color.Red;
+                    C3.BackColor = Color.Red;
+                    D3.BackColor = Color.Red;
+                    E3.BackColor = Color.Gainsboro;
+                    F3.BackColor = Color.Gainsboro;
+                    G3.BackColor = Color.Red;
+                    break;
+
+                case "4":
+                    A3.BackColor = Color.Gainsboro;
+                    B3.BackColor = Color.Red;
+                    C3.BackColor = Color.Red;
+                    D3.BackColor = Color.Gainsboro;
+                    E3.BackColor = Color.Gainsboro;
+                    F3.BackColor = Color.Red;
+                    G3.BackColor = Color.Red;
+                    break;
+
+                case "5":
+                    A3.BackColor = Color.Red;
+                    B3.BackColor = Color.Gainsboro;
+                    C3.BackColor = Color.Red;
+                    D3.BackColor = Color.Red;
+                    E3.BackColor = Color.Gainsboro;
+                    F3.BackColor = Color.Red;
+                    G3.BackColor = Color.Red;
+                    break;
+
+                case "6":
+                    A3.BackColor = Color.Red;
+                    B3.BackColor = Color.Gainsboro;
+                    C3.BackColor = Color.Red;
+                    D3.BackColor = Color.Red;
+                    E3.BackColor = Color.Red;
+                    F3.BackColor = Color.Red;
+                    G3.BackColor = Color.Red;
+                    break;
+
+                case "7":
+                    A3.BackColor = Color.Red;
+                    B3.BackColor = Color.Red;
+                    C3.BackColor = Color.Red;
+                    D3.BackColor = Color.Gainsboro;
+                    E3.BackColor = Color.Gainsboro;
+                    F3.BackColor = Color.Gainsboro;
+                    G3.BackColor = Color.Gainsboro;
+                    break;
+
+                case "8":
+                    A3.BackColor = Color.Red;
+                    B3.BackColor = Color.Red;
+                    C3.BackColor = Color.Red;
+                    D3.BackColor = Color.Red;
+                    E3.BackColor = Color.Red;
+                    F3.BackColor = Color.Red;
+                    G3.BackColor = Color.Red;
+                    break;
+
+                case "9":
+                    A3.BackColor = Color.Red;
+                    B3.BackColor = Color.Red;
+                    C3.BackColor = Color.Red;
+                    D3.BackColor = Color.Red;
+                    E3.BackColor = Color.Gainsboro;
+                    F3.BackColor = Color.Red;
+                    G3.BackColor = Color.Red;
+                    break;
+            }
+
+
+            switch (Convert.ToString(Num[1]))
+            {
+                case "0":
+                    A2.BackColor = Color.Red;
+                    B2.BackColor = Color.Red;
+                    C2.BackColor = Color.Red;
+                    D2.BackColor = Color.Red;
+                    E2.BackColor = Color.Red;
+                    F2.BackColor = Color.Red;
+                    G2.BackColor = Color.Gainsboro;
+                    break;
+
+
+                case "1":
+                    A2.BackColor = Color.Gainsboro;
+                    B2.BackColor = Color.Red;
+                    C2.BackColor = Color.Red;
+                    D2.BackColor = Color.Gainsboro;
+                    E2.BackColor = Color.Gainsboro;
+                    F2.BackColor = Color.Gainsboro;
+                    G2.BackColor = Color.Gainsboro;
+                    break;
+
+                case "2":
+                    A2.BackColor = Color.Red;
+                    B2.BackColor = Color.Red;
+                    C2.BackColor = Color.Gainsboro;
+                    D2.BackColor = Color.Red;
+                    E2.BackColor = Color.Red;
+                    F2.BackColor = Color.Gainsboro;
+                    G2.BackColor = Color.Red;
+                    break;
+
+                case "3":
+                    A2.BackColor = Color.Red;
+                    B2.BackColor = Color.Red;
+                    C2.BackColor = Color.Red;
+                    D2.BackColor = Color.Red;
+                    E2.BackColor = Color.Gainsboro;
+                    F2.BackColor = Color.Gainsboro;
+                    G2.BackColor = Color.Red;
+                    break;
+
+                case "4":
+                    A2.BackColor = Color.Gainsboro;
+                    B2.BackColor = Color.Red;
+                    C2.BackColor = Color.Red;
+                    D2.BackColor = Color.Gainsboro;
+                    E2.BackColor = Color.Gainsboro;
+                    F2.BackColor = Color.Red;
+                    G2.BackColor = Color.Red;
+                    break;
+
+                case "5":
+                    A2.BackColor = Color.Red;
+                    B2.BackColor = Color.Gainsboro;
+                    C2.BackColor = Color.Red;
+                    D2.BackColor = Color.Red;
+                    E2.BackColor = Color.Gainsboro;
+                    F2.BackColor = Color.Red;
+                    G2.BackColor = Color.Red;
+                    break;
+
+                case "6":
+                    A2.BackColor = Color.Red;
+                    B2.BackColor = Color.Gainsboro;
+                    C2.BackColor = Color.Red;
+                    D2.BackColor = Color.Red;
+                    E2.BackColor = Color.Red;
+                    F2.BackColor = Color.Red;
+                    G2.BackColor = Color.Red;
+                    break;
+
+                case "7":
+                    A2.BackColor = Color.Red;
+                    B2.BackColor = Color.Red;
+                    C2.BackColor = Color.Red;
+                    D2.BackColor = Color.Gainsboro;
+                    E2.BackColor = Color.Gainsboro;
+                    F2.BackColor = Color.Gainsboro;
+                    G2.BackColor = Color.Gainsboro;
+                    break;
+
+                case "8":
+                    A2.BackColor = Color.Red;
+                    B2.BackColor = Color.Red;
+                    C2.BackColor = Color.Red;
+                    D2.BackColor = Color.Red;
+                    E2.BackColor = Color.Red;
+                    F2.BackColor = Color.Red;
+                    G2.BackColor = Color.Red;
+                    break;
+
+                case "9":
+                    A2.BackColor = Color.Red;
+                    B2.BackColor = Color.Red;
+                    C2.BackColor = Color.Red;
+                    D2.BackColor = Color.Red;
+                    E2.BackColor = Color.Gainsboro;
+                    F2.BackColor = Color.Red;
+                    G2.BackColor = Color.Red;
+                    break;
+            }
+
+            switch (Convert.ToString(Num[2]))
+            {
+                case "0":
+                    A1.BackColor = Color.Red;
+                    B1.BackColor = Color.Red;
+                    C1.BackColor = Color.Red;
+                    D1.BackColor = Color.Red;
+                    E1.BackColor = Color.Red;
+                    F1.BackColor = Color.Red;
+                    G1.BackColor = Color.Gainsboro;
+                    break;
+
+
+                case "1":
+                    A1.BackColor = Color.Gainsboro;
+                    B1.BackColor = Color.Red;
+                    C1.BackColor = Color.Red;
+                    D1.BackColor = Color.Gainsboro;
+                    E1.BackColor = Color.Gainsboro;
+                    F1.BackColor = Color.Gainsboro;
+                    G1.BackColor = Color.Gainsboro;
+                    break;
+
+                case "2":
+                    A1.BackColor = Color.Red;
+                    B1.BackColor = Color.Red;
+                    C1.BackColor = Color.Gainsboro;
+                    D1.BackColor = Color.Red;
+                    E1.BackColor = Color.Red;
+                    F1.BackColor = Color.Gainsboro;
+                    G1.BackColor = Color.Red;
+                    break;
+
+                case "3":
+                    A1.BackColor = Color.Red;
+                    B1.BackColor = Color.Red;
+                    C1.BackColor = Color.Red;
+                    D1.BackColor = Color.Red;
+                    E1.BackColor = Color.Gainsboro;
+                    F1.BackColor = Color.Gainsboro;
+                    G1.BackColor = Color.Red;
+                    break;
+
+                case "4":
+                    A1.BackColor = Color.Gainsboro;
+                    B1.BackColor = Color.Red;
+                    C1.BackColor = Color.Red;
+                    D1.BackColor = Color.Gainsboro;
+                    E1.BackColor = Color.Gainsboro;
+                    F1.BackColor = Color.Red;
+                    G1.BackColor = Color.Red;
+                    break;
+
+                case "5":
+                    A1.BackColor = Color.Red;
+                    B1.BackColor = Color.Gainsboro;
+                    C1.BackColor = Color.Red;
+                    D1.BackColor = Color.Red;
+                    E1.BackColor = Color.Gainsboro;
+                    F1.BackColor = Color.Red;
+                    G1.BackColor = Color.Red;
+                    break;
+
+                case "6":
+                    A1.BackColor = Color.Red;
+                    B1.BackColor = Color.Gainsboro;
+                    C1.BackColor = Color.Red;
+                    D1.BackColor = Color.Red;
+                    E1.BackColor = Color.Red;
+                    F1.BackColor = Color.Red;
+                    G1.BackColor = Color.Red;
+                    break;
+
+                case "7":
+                    A1.BackColor = Color.Red;
+                    B1.BackColor = Color.Red;
+                    C1.BackColor = Color.Red;
+                    D1.BackColor = Color.Gainsboro;
+                    E1.BackColor = Color.Gainsboro;
+                    F1.BackColor = Color.Gainsboro;
+                    G1.BackColor = Color.Gainsboro;
+                    break;
+
+                case "8":
+                    A1.BackColor = Color.Red;
+                    B1.BackColor = Color.Red;
+                    C1.BackColor = Color.Red;
+                    D1.BackColor = Color.Red;
+                    E1.BackColor = Color.Red;
+                    F1.BackColor = Color.Red;
+                    G1.BackColor = Color.Red;
+                    break;
+
+                case "9":
+                    A1.BackColor = Color.Red;
+                    B1.BackColor = Color.Red;
+                    C1.BackColor = Color.Red;
+                    D1.BackColor = Color.Red;
+                    E1.BackColor = Color.Gainsboro;
+                    F1.BackColor = Color.Red;
+                    G1.BackColor = Color.Red;
+                    break;
+            }
+        }
+    }
+}
